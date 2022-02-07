7c55c4598ec0692097db19bb025fa54afe547972
Yashwendra <yashwendrapratapsingh1899@gmail.com>
2022-02-07 16:25:57 +0530

    first commit

--- src/Components/DayActivity/DayWorkList/DayWorkList.js
+++ src/Components/DayActivity/DayWorkList/DayWorkList.js
@@ -1,16 +1,12 @@
-import React from 'react';
-import DayWorkItem from '../DayWorkItem/DayWorkItem';
-import './DayWorkList.css';
+import React from "react";
+import DayWorkItem from "../DayWorkItem/DayWorkItem";
+import "./DayWorkList.css";
 
-const DayWorkList = props => {
+const DayWorkList = (props) => {
   return (
     <ul className="work-list">
-      {props.items.map(work => (
-        <DayWorkItem
-          key={work.id}
-          id={work.id}
-          onDelete={props.onDeleteItem}
-        >
+      {props.items.map((work) => (
+        <DayWorkItem key={work.id} id={work.id} onDelete={props.onDeleteItem}>
           {work.text}
         </DayWorkItem>
       ))}
